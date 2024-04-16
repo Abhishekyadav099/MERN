0c61d661386182c1c8bf47149f2cb3fc40dddd97
Abhishekyadav099 <149036053+Abhishekyadav099@users.noreply.github.com>
2024-04-16 15:38:03 +0530

    Update

--- Day8/server_script.js
+++ Day8/server_script.js
@@ -5,43 +5,100 @@ const data = fs.readFileSync("./data.json", "utf8");
 const dataObj = JSON.parse(data).products;
 // console.log(Objdata);
 const cardTemplate = `
-    <div class='product-card'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <style>
+    .product-card {
+        background-color: aqua;
+        max-width: 400px;
+        padding: 24px;
+        margin: 20px auto;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);   
+    }
+    .product-card img {
+        max-width: 100%;
+        height: auto;
+        display: block;
+        margin: 0 auto 16px;
+        border-radius: 8px;
+    }
+    
+    .product-card {
+        flex-wrap: wrap;
+        // display: flex;
+        justify-content: space-between;
+    }
+    h3 {
+        text-decoration: underline;
+        text-align: center;
+        font-size: 40px;
+        color:red;
+}
+.heading{
+    
+}
+</style>
+</head>
+<body>
+
+     <div class='product-card'>
         <h3>$TITLES</h3>
         <img src="$img_src5" alt='product-image' >
+        <h2>$price</h2>
+        <p>$brand</p>
+        <p>$stock</p>
+        <p>$category</p>
+        <p>$discountPercentage</p>
         <a href="$product_links">More info</a>
-    </div>`;
+    </div>
+</body>
+</html>`
+    
+   
 
 
 let result = [];
-for(let i=0; i<dataObj.length; i++){
+for (let i = 0; i < dataObj.length; i++) {
     let temp = cardTemplate;
-    temp = temp.replace('$TITLES',dataObj[i].title);
-    temp = temp.replace('$img_src5',dataObj[i].images[0]);
-    temp = temp.replace('$product_links', `/product?id=${i+1}`);
+    temp = temp.replace('$TITLES', dataObj[i].title);
+    temp = temp.replace('$img_src5', dataObj[i].images[0]);
+    temp = temp.replace('$price', dataObj[i].price);
+    temp = temp.replace('$discountPercentage', dataObj[i].discountPercentage);
+    temp = temp.replace('$brand', dataObj[i].brand);
+    temp = temp.replace('$stock', dataObj[i].stock);
+    temp = temp.replace('$category', dataObj[i].category);
+    temp = temp.replace('$product_links', `/product?id=${i}`);
 
     result.push(temp);
+
 }
 result = result.join(' ');
 
 
 // console.log(result);
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // const route = req.url;
     // console.log(route);
     // const path = url.parse(req.url);
     // const pathname = path.pathname;
-    const {pathname, query} = url.parse(req.url, true);
+    const { pathname, query } = url.parse(req.url, true);
     console.log(query);
 
     // const path = url.parse(req.url).pathname
     // console.log(path);
 
-    if (pathname=='/home') {
+    if (pathname == '/home') {
         res.end(result)
     }
-    else if (pathname=='/product') {
+    else if (pathname == '/product') {
         const id = query.id;
         // const productid = dataObj[id].title;
         // res.end(productid)
@@ -53,18 +110,21 @@ const server = http.createServer((req, res) => {
                 <h4>${items.id}</h4>
                 <h2>${items.price}</h2>
                 <h2>${items.description}</h2>
+                <p>${items.brand}</p>
+                <p>${items.stock}</p>
+                <p>${items.category}</p>
                 <img src="${items.thumbnail}" alt="asdf"></img>
-                
+                <a href="/home">Back</a>
             </div>
         `);
     }
-    else{
+    else {
         res.end('404... Not Found')
     }
 
     // res.end(result);
 })
 
-server.listen(1400,()=>{
+server.listen(1400, () => {
     console.log("....Server.....");
 });
---
+++ Day9/node/server_script.js
@@ -0,0 +1,138 @@
+const http = require('http')
+const fs = require('fs');
+const url = require('node:url');
+const { title } = require('process');
+
+const data = fs.readFileSync('data.json', 'utf-8')
+// console.log(data);
+
+const dataObj = JSON.parse(data).products;
+
+
+const searchBox = `
+<div style="margin-bottom: 20px;">
+
+<form action='/product'>
+  <input type="text" name="productName">
+  <button type="submit">Search</button>
+</form>
+  </div>
+`;
+
+
+const cardTemplate = `
+<div class="product-card" style="border: 1px solid #ccc; padding: 10px; margin-bottom: 20px;  display: flex;
+justify-content: center; flex-direction:column; height:auto; width:500px; align-items:center;">
+<h3>$TITLE</h3>
+<img src="img_src" alt="product-images" style="max-width: 100%; height: auto;">
+<p>$PRICE</p>
+<p>$Brand</p>
+<a href="/product_links" >Read more</a>
+</div>`
+
+let result = [];
+for (let i = 0; i < dataObj.length; i++) {
+
+  let temp = cardTemplate;
+  temp = temp.replace('$TITLE', dataObj[i].title)
+  temp = temp.replace('img_src', dataObj[i].images[0])
+  temp = temp.replace('$Brand', dataObj[i].brand)
+  temp = temp.replace('$PRICE', dataObj[i].price)
+  temp = temp.replace('product_links', `product?id=${i}`);
+  result.push(temp);
+}
+
+result = result.join('');
+
+result = searchBox + result;
+
+// console.log(result);
+const server = http.createServer((req, res) => {
+
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+
+  const { pathname, query } = url.parse(req.url, true);
+  // console.log(route, "\n\n");
+  // console.log(path);
+  // console.log(query);
+
+  if (pathname == '/home') {
+    res.end(result);
+
+  }
+  else if (pathname == '/product') {
+    const id = query.id;
+    // console.log(dataObj[id]);
+    const items = dataObj[id];
+    const pName = query.productName
+    if (id) {
+
+
+      // res.end(productid);
+      res.end(`<div style="max-width: 800px; margin: 0 auto; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(255, 0, 0, 0.2);">
+        <style>
+          h1 {
+            text-align: center;
+          }
+          h2, p {
+            color: black;
+          }
+          img {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 20px;
+            border-radius: 8px;
+            display: block;
+          }
+          a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: blue;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+          }
+      
+          .buy-now {
+            margin-top: 20px;
+            text-align: center;
+          }
+          .buy-now a {
+            background-color: green;
+          }
+     
+        </style>
+        <h1>Product Display Page</h1>
+        <h1>Item ID-${items.id}</h1>
+        <h2>Price - ${items.price}</h2>
+        <img src="${items.thumbnail}" alt="Product Image">
+        <p>${items.description}</p>
+        <a href="/home">Back to Products Page</a>
+        <div class="buy-now">
+          <a >Buy Now</a>
+        </div>
+      </div>`);
+
+    }
+    else if (pName) {
+      const SearchNameResult = dataObj.filter(({ title }) => title.toLowerCase().includes(pName.toLowerCase()));
+      res.end(JSON.stringify(SearchNameResult));
+    }
+    else {
+      res.end('<h3>Error......</h3>')
+    }
+
+  }
+  else {
+    res.end("404 page not found");
+  }
+
+  // res.end(result);
+
+});
+server.listen(1400, () => {
+  console.log("server started.....");
+})
