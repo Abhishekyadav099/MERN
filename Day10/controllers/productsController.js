48848a6f3ba3d93545bcb5eb4d6453fc0d55c787
Abhishekyadav099 <149036053+Abhishekyadav099@users.noreply.github.com>
2024-04-18 09:11:48 +0530

    Day 10

---
+++ Day10/controllers/productsController.js
@@ -0,0 +1,12 @@
+const fsPromises = require("fs/promises")
+
+const getAllProducts = async(req, res)=>{
+}
+
+const addProduct = async (req, res)=>{
+};
+
+module.exports = {
+    getAllProducts,
+    addProduct,
+}
